6f9fe656a4b37e900709e81b069c2d5e9c783b04
Vasily Vasinov <des.elyon@gmail.com>
2021-04-14 17:38:19 -0600

    Add LICENSE, update setup.py, move automation Lambda image Docker files out of the package

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
-from setuptools import setup, find_packages
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
-    name="imp",
+    name="chaosimp",
     version="0.1.0",
     description="Chaos engineering on AWS",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/chaosops/imp",
     author="Vasily Vasinov",
     author_email="vasinov@me.com",
@@ -17,6 +23,6 @@ setup(
         "pyhumps>=1.6.0"
     ],
     entry_points={
-        "console_scripts": ["imp=chaosimp.main:main"]
+        "console_scripts": ["imp=chaosimp.__main__:main"]
     }
 )
